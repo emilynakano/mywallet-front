6ec4154a858b05b8db115de636bf5d27b3cc1abe
emily <nakanodepaula@gmail.com>
2022-06-28 19:55:04 -0300

    routes ready

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-//import LoginPage from './LoginPage'
-//import RegisterPage from './RegisterPage'
-//import InitialPage from './InitialPage'
-//import AddRegisterPage from './AddRegisterPage'
-import RemoveRegisterPage from './RemoveRegisterPage'
+import LoginPage from './LoginPage'
+import RegisterPage from './RegisterPage'
+import InitialPage from './InitialPage'
+import AddRegisterPage from './AddRegisterPage'
+import ExitRegisterPage from './ExitRegisterPage'
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<RemoveRegisterPage/>} />
+                <Route path="/" element={<LoginPage/>} />
+                <Route path="/register" element={<RegisterPage/>} />
+                <Route path="/home" element={<InitialPage/>} />
+                <Route path="/add" element={<AddRegisterPage/>} />
+                <Route path="/exit" element={<ExitRegisterPage/>} />
             </Routes>
         </BrowserRouter>
     )
