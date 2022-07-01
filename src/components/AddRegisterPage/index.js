7f13427dafd4b2167ae21af2ec31968529ebc6f1
emily <nakanodepaula@gmail.com>
2022-06-30 23:18:56 -0300

    send the inputs and outputs to the API

--- src/components/AddRegisterPage/index.js
+++ src/components/AddRegisterPage/index.js
@@ -1,26 +1,38 @@
 import { Container } from "./style"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import axios from 'axios'
+
+import { useContext } from "react"
+import UserContext from "../../contexts/UserContext"
+
 export default function AddRegisterPage () {
+    const {user} = useContext(UserContext)
     const navigate = useNavigate()
-    const [entrada, setEntrada] = useState({
+    const [input, setInput] = useState({
         value: '',
         description: ''
     })
     function ChangeInput (e) {
-        setEntrada({...entrada, [e.target.name]: e.target.value})
+        setInput({...input, [e.target.name]: e.target.value})
     }
-    function Save(e) {
+    function HandleSubmit(e) {
         e.preventDefault();
-        navigate('/home')
+        const config = {
+            headers: {
+                Authorization: `Bearer ${user.token}`
+            }
+        }
+        const promise = axios.post('http://localhost:5000/post', {...input, type: 'add'}, config);
+        promise.then(() => navigate('/home'));
     }
     return (
         <Container>
             <h2>Nova entrada</h2>
             <form >
-                <input type="number" placeholder="Valor" name="value" value={entrada.value} onChange={ChangeInput}/>
-                <input type="text" placeholder="Descrição" name="description" value={entrada.description} onChange={ChangeInput}/>
-                <button onClick={Save} type="submit">
+                <input type="number" placeholder="Valor" name="value" value={input.value} onChange={ChangeInput}/>
+                <input type="text" placeholder="Descrição" name="description" value={input.description} onChange={ChangeInput}/>
+                <button onClick={HandleSubmit}>
                     <span>Salvar entrada</span>
                 </button>
             </form>
--- src/components/ExitRegisterPage/index.js
+++ src/components/ExitRegisterPage/index.js
@@ -1,26 +1,37 @@
 import {Container} from '../AddRegisterPage/style'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import UserContext from '../../contexts/UserContext'
+import axios from 'axios'
+
 export default function RemoveRegisterPage () {
+    const {user} = useContext(UserContext)
     const navigate = useNavigate()
-    const [exit, setExit] = useState({
+    const [output, setOutput] = useState({
         value:'',
         description: ''
     })
     function ChangeInput (e) {
-        setExit({...exit, [e.target.name]: e.target.value})
+        setOutput({...output, [e.target.name]: e.target.value})
     }
-    function Save(e) {
+    function HandleSubmit(e) {
         e.preventDefault();
-        navigate('/home')
+        const config = {
+            headers: {
+                Authorization: `Bearer ${user.token}`
+            }
+        }
+        const promise = axios.post('http://localhost:5000/post', {...output, type: 'exit'}, config);
+        promise.then(() => navigate('/home'));
     }
     return (
         <Container>
             <h2>Nova saída</h2>
             <form >
-                <input type="number" placeholder="Valor" name="value" value={exit.value} onChange={ChangeInput}/>
-                <input type="text" placeholder="Descrição" name='description' value={exit.description} onChange={ChangeInput}/>
-                <button type='submit' onClick={Save}>
+                <input type="number" placeholder="Valor" name="value" value={output.value} onChange={ChangeInput}/>
+                <input type="text" placeholder="Descrição" name='description' value={output.description} onChange={ChangeInput}/>
+                <button type='submit' onClick={HandleSubmit}>
                     <span>Salvar saída</span>
                 </button>
             </form>
